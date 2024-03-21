855b4c33cdcefa86722fbd8cfeac80a1ab4b97d9
Daniel Luiz <danielluizdossantos12@gmail.com>
2024-03-21 14:07:53 -0300

    Created Project

---
+++ src/main/java/com/Daniel/aulaJavaBoot/AulaJavaBootApplication.java
@@ -0,0 +1,13 @@
+package com.Daniel.aulaJavaBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJavaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJavaBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Daniel/aulaJavaBoot/AulaJavaBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Daniel.aulaJavaBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJavaBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
